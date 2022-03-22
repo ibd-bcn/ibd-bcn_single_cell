c8cdf44bc5ae8d7afcca8d14012823709624044f
Ana <acorrama@gmail.com>
2022-03-22 16:54:04 +0000

    colors for figures

---
+++ source/colors.R
@@ -0,0 +1,25 @@
+
+colors_subset <- c('epi' = '#CBA6DD',
+                   'stroma' =  '#9AC772',
+                   'tcells' = '#246996',
+                   'plasmas' = '#E1BB55',
+                   'myeloids' = '#A05B52')
+
+colors_health <- c('HC' =  '#71AD96',
+                   'CDa' = '#525174',
+                   'UCa' = '#CBA552')
+
+colors_health_alpha <- c('HC' =  '#71AD9680',
+                         'CDa' = '#52517480',
+                         'UCa' = '#CBA55280')
+
+cols_intermediate <- c('Mast' = "#FDBF6F",
+                       'Eosinophils' ="#E7298A",
+                       'Neutrophil' = "#A6CEE3",
+                       'Macrophage NRG1' = "#1F78B4",
+                       'M0' = "#33A02C",
+                       'M1' = "#E31A1C" ,
+                       'Inflammatory monocytes' = "#FB9A99",
+                       'M2' = "#CAB2D6",
+                       'DCs' = "#A6761D" ,
+                       'Cycling myeloid' = "#E6AB02")
