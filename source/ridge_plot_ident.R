ec2d9a04b90e698817979ecc66285011aabcb4b3
Ana <acorrama@gmail.com>
2022-03-22 16:05:01 +0000

    function ridge

--- source/ridge_plot_ident.R
+++ source/ridge_plot_ident.R
@@ -11,6 +11,7 @@ ridge_plot_one_ident <- function(object, features,
   library(scales)
   library(ggridges)
   require(cowplot)
+  require(reshape2)
   
   # 
   # checkpoints
@@ -22,286 +23,107 @@ ridge_plot_one_ident <- function(object, features,
   if (length(idents) > 1){
     stop('ONLY ONE IDENT WITH THIS FUNCTION')
   }
+  
+  if(is.null(x = group.by)){stop('group.by IS MANDATORY')}
+  
 if (!is.null(x = group.by)) {
   object <- SetIdent(object, value = group.by)
   if(idents %in% levels(object)){
     object <- subset(object, idents = idents)
   }else{
-    stop('IDENT NOT PRESENT IN THE GROUP.BY')
+    stop('idents NOT PRESENT IN group.by')
+  }}
+
+#
+# select cells
+#
+  cells <- names(x = Idents(object = object)[Idents(object = object) %in% idents])
+  idents <-object[[group.by, drop = TRUE]][cells]
+  
+  if (!is.factor(x = idents)) {
+    idents <- factor(x = idents)
+  }else{
+    idents <- droplevels(idents)
   }
   
+  if (is.null(x = colors)) {
+    colors <- scales::hue_pal()(length(x = levels(x = idents)))
+    colors <- alpha(colors, alpha = 0.5)
+  } else {
+    colors <- Col2Hex(colors)
+  }
+
+  y <- 'ident'
+  xlab <- 'Expression Level'
+  ylab <- 'Identity'
   
-}
+  data <- FetchData(object, vars = c(features, split.by, group.by),
+                    cells = cells, slot = 'data')
 
-#
-# select cells!
-#
-if (is.null(x = idents)) {
-  cells <- colnames(x = object)
-} else {
-  cells <- names(x = Idents(object = object)[Idents(object = object) %in% idents])
-}
-
-idents <- if (is.null(x = group.by)) {
-  Idents(object = object)[cells]
-} else {
-  object[[group.by, drop = TRUE]][cells]
-}
-if (!is.factor(x = idents)) {
-  idents <- droplevels(factor(x = idents))
-}
-
-idents <- droplevels(factor(x = idents, levels = levels(idents)[order(levels(idents))]))
-if (is.null(x = colors)) {
-  colors <- scales::hue_pal()(length(x = levels(x = idents)))
-  colors <- alpha(colors, alpha = 0.5)
-} else {
-  colors <- Col2Hex(colors)
-}
-
-y <- 'ident'
-xlab <- 'Expression Level'
-ylab <- 'Identity'
-
-data <- FetchData(object, vars = c(features, split.by, group.by),
-                  cells = cells, slot = 'data')
-
-data[,group.by] <- factor( data[,group.by] , levels = levels(idents))
-if(is.null(group.by)){
-  data <- cbind(data, object@active.ident)
-  group.by <- 'object@active.ident'
-}
+  data[,group.by] <- factor(data[,group.by] , levels = levels(idents))
+  data1 <- melt(data)
+  head(data1)
 
 if(is.null(split.by)){
-  if(length(features) == 1){
-    plot <- ggplot(data, aes_string(x=features, 
-                                    y=group.by,
-                                    fill = group.by)) +
+  plot <- ggplot(data1, aes_string(x='value',
+                                     y='variable',
+                                     fill = group.by)) +
       geom_density_ridges()+
       theme_ridges(grid = FALSE, center_axis_labels = TRUE)+
       theme(axis.title.y = element_blank(),
             axis.title.x = element_blank())+
       scale_x_continuous(expand = c(0, 0)) +
       scale_y_discrete(expand = c(0, 0)) +
-      coord_cartesian(clip = "off") + 
-      labs(title = features)
-    
-  }else{
-    plot <- NULL
-    list_plot <- vector(mode = "list", length = length(features))
-    names(list_plot) <- features
-    for(feature in features){
-      if(feature != features[length(features)]){
-        a <- ggplot(data, aes_string(x=add.backtick(feature), 
-                                     y=group.by,
-                                     fill = group.by))  + 
-          geom_density_ridges() +
-          labs(title = feature) +
-          theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-          theme(axis.title.x = element_blank(),
-                axis.title.y = element_blank(),
-                strip.background = element_rect(fill = '#4287f500')) +
-          scale_x_continuous(expand = c(0, 0)) +
-          scale_y_discrete(expand = c(0, 0)) +
-          coord_cartesian(clip = "off")
-        list_plot[[feature]] <- a}
-      else{
-        a <- ggplot(data, aes_string(x=add.backtick(feature), 
-                                     y=group.by,
-                                     fill = group.by))  + 
-          geom_density_ridges() +
-          labs(title = feature)+
-          theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-          theme(
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank())+
-          scale_x_continuous(expand = c(0, 0)) +
-          scale_y_discrete(expand = c(0, 0)) +
-          coord_cartesian(clip = "off")
-        list_plot[[feature]] <- a
-        
-      }
-    }
-    plot <- wrap_plots(list_plot, ncol = length(features),
-                       guides = 'collect')
+      coord_cartesian(clip = "off") +
+      labs(title = levels(idents))
   }
-  
-}
-#
-# split by!
-#
 
-if(!is.null(split.by)){
-  if(length(split.by) == 1){
+  #
+  # split by!
+  #
+# 
+  if(!is.null(split.by)){
+    colnames(data1)[colnames(data1) %in% split.by] <- make.unique(rep('split.by', length(split.by)))
+    print(head(data1))
     #
     # ONE SPLIT.BY
     #
-    colnames(data)[colnames(data) %in% split.by] <- make.unique(rep('split.by', length(split.by)))
-    if(length(features) == 1){
-      plot <- ggplot(data, aes_string(x=add.backtick(features), 
-                                      y=group.by, fill = group.by)) + 
-        geom_density_ridges() +
-        labs(title = features)+
-        theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-        theme(axis.title.x = element_blank(),
-              axis.title.y = element_blank())+
-        facet_grid(cols = vars(split.by), scales = 'free')
+    if(length(split.by) == 1){
+      plot <- ggplot(data1, aes(x=value,
+                               y=variable,
+                               fill = split.by)) +
+          geom_density_ridges() +
+          labs(title = levels(idents))+
+          theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
+          theme(axis.title.x = element_blank(),
+                axis.title.y = element_blank())
     }
-    if(length(features) > 1){
-      plot <- NULL
-      list_plot <- vector(mode = "list", length = length(features))
-      names(list_plot) <- features
-      for(feature in features){
-        if(feature == features[1]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by), scales = 'free') +
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background = element_rect(fill = '#4287f500'))
-          
-          list_plot[[feature]] <- a
-        }
-        if(feature != features[length(features)] &
-           feature != features[1]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by), scales = 'free')+
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background = element_blank(),
-                  strip.text = element_blank()
-            )
-          
-          list_plot[[feature]] <- a
-          
-        }
-        if(feature == features[length(features)]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by), scales = 'free')+
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background = element_blank(),
-                  strip.text = element_blank()
-            )
-          list_plot[[feature]] <- a
-        }
-      }
-      plot <- wrap_plots(list_plot, nrow = length(features),
-                         guides = 'collect')
-    }
-  }
-  if(length(split.by) > 1){
     #
     # MULTIPLE SPLIT.BY
     #
-    colnames(data)[colnames(data) %in% split.by] <- make.unique(rep('split.by', length(split.by)))
-    if(length(features) == 1){
-      plot <- ggplot(data, aes_string(x=add.backtick(features), 
-                                      y=group.by, fill = group.by)) + 
-        geom_density_ridges() +
-        labs(title = feature)+
-        theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-        theme(axis.title.x = element_blank(),
-              axis.title.y = element_blank())+
-        facet_grid(cols = vars(split.by),
-                   rows = vars(split.by.1),
-                   scales = 'free')
-    }
-    if(length(features) > 1){
-      plot <- NULL
-      list_plot <- vector(mode = "list", length = length(features))
-      names(list_plot) <- features
-      for(feature in features){
-        if(feature == features[1]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by),
-                       rows = vars(split.by.1),
-                       scales = 'free')+
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background = element_rect(fill = '#4287f500'))
-          
-          list_plot[[feature]] <- a
+    if(length(split.by) > 1){
+      plot <- ggplot(data1, aes_string(x='value',
+                                      y='variable',
+                                      fill = 'split.by')) +
+              geom_density_ridges() +
+              labs(title = levels(idents))+
+              theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
+              theme(axis.title.x = element_blank(),
+                    axis.title.y = element_blank())+
+              facet_grid(cols = vars(split.by.1),
+                         scales = 'free')
         }
-        if(feature != features[length(features)] &
-           feature != features[1]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by),
-                       rows = vars(split.by.1),
-                       scales = 'free')+
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background.x = element_blank(),
-                  strip.text.x = element_blank()
-            )
-          
-          list_plot[[feature]] <- a
-          
-        }
-        if(feature == features[length(features)]){
-          a <-ggplot(data, aes_string(x=add.backtick(feature), 
-                                      y=group.by,
-                                      fill = group.by)) + 
-            geom_density_ridges() +
-            labs(title = feature)+
-            theme_ridges(grid = FALSE, center_axis_labels = TRUE) +
-            facet_grid(cols = vars(split.by),
-                       rows = vars(split.by.1),
-                       scales = 'free')+
-            theme(axis.title.x = element_blank(),
-                  axis.title.y = element_blank(),
-                  strip.background.x = element_blank(),
-                  strip.text.x = element_blank()
-            )
-          list_plot[[feature]] <- a
-        }
+
       }
-      plot <- wrap_plots(list_plot, nrow = length(features),
-                         guides = 'collect')
-    }
-  }
-  
-}
 
-if(group.by == 'object@active.ident'){
-  names(colors) <- NULL
-  plot <- plot+
-    scale_fill_manual(values = colors) + 
-    labs(y = 'Idents')+
-    NoLegend()
-}
-
-plot <- plot &
-  scale_x_continuous(expand = c(0, 0)) &
-  scale_y_discrete(expand = c(0, 0)) &
-  coord_cartesian(clip = "off") &
+plot <- plot +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  coord_cartesian(clip = "off") +
   theme(strip.background.y = element_rect(fill = '#4287f500'),
         strip.background.x = element_rect(fill = '#4287f500'))
 
 return(plot)
 
 }
+
