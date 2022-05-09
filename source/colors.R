afca8229f0ebdc9cb98316e3eb800ed3fa0a7ce2
Ana <acorrama@gmail.com>
2022-05-09 10:09:22 +0000

    Add necessary colors.

--- source/colors.R
+++ source/colors.R
@@ -1,25 +1,34 @@
-
-colors_subset <- c('epi' = '#CBA6DD',
-                   'stroma' =  '#9AC772',
-                   'tcells' = '#246996',
-                   'plasmas' = '#E1BB55',
-                   'myeloids' = '#A05B52')
-
-colors_health <- c('HC' =  '#71AD96',
-                   'CDa' = '#525174',
-                   'UCa' = '#CBA552')
+cols <- c("#A6CEE3","#1F78B4", 
+          "#B2DF8A", "#33A02C",
+          "#FB9A99", "#E31A1C",
+          "#FDBF6F","#FF7F00",
+          "#CAB2D6", "#6A3D9A", 
+          "#FFFF99", "#B15928", 
+          "#1B9E77", "#D95F02",
+          "#7570B3", "#E7298A", 
+          "#66A61E", "#E6AB02", 
+          "#A6761D", "#666666", 
+          "aquamarine", "aquamarine4", 
+          "beige", "bisque4",
+          "lightpink", "lightpink3",
+          "slateblue1", "slateblue4", 
+          "tan1", "tan4", "purple4", 
+          "purple1", "lightsalmon4",
+          "lightsalmon","maroon4",
+          "maroon1", "lemonchiffon4", 
+          "lemonchiffon")
+           
+colors_subset <- readRDS('source/colors_subset.RDS')
+colors_health <- readRDS('source/colors_health.RDS')
+cols_epi <- readRDS('source/cols_epi.RDS')
+cols_myeloids <- readRDS('source/cols_myeloids.RDS')
+cols_plasmas <- readRDS('source/cols_plasmas.RDS')
+cols_stroma <- readRDS('source/cols_stroma.RDS')
+cols_tcells <- readRDS('source/cols_tcells.RDS')
+cols_myeloids_intermediate <- readRDS('source/cols_myeloids_intermediate.RDS')
 
 colors_health_alpha <- c('HC' =  '#71AD9680',
                          'CDa' = '#52517480',
                          'UCa' = '#CBA55280')
 
-cols_intermediate <- c('Mast' = "#FDBF6F",
-                       'Eosinophils' ="#E7298A",
-                       'Neutrophil' = "#A6CEE3",
-                       'Macrophage NRG1' = "#1F78B4",
-                       'M0' = "#33A02C",
-                       'M1' = "#E31A1C" ,
-                       'Inflammatory monocytes' = "#FB9A99",
-                       'M2' = "#CAB2D6",
-                       'DCs' = "#A6761D" ,
-                       'Cycling myeloid' = "#E6AB02")
+
